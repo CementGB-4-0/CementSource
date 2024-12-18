596d5d705946e59dea0597946c62c4d6e4a9d687
TheUltimateNuke <nuke.public@gmail.com>
2024-12-18 10:29:55 -0500

    fixed LoggingUtilities not showing correct caller names

--- CementMod/src/Utilities/LoggingUtilities.cs
+++ CementMod/src/Utilities/LoggingUtilities.cs
@@ -1,5 +1,5 @@
-using MelonLoader;
 using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace CementGB.Mod.Utilities;
@@ -9,11 +9,20 @@ internal static class LoggingUtilities
     internal static void VerboseLog(ConsoleColor color, string message, [CallerMemberName] string callerName = null, [CallerLineNumber] int lineNumber = 0)
     {
         if (!CementPreferences.VerboseMode) return;
-        Mod.Logger.Msg(color, callerName == null ? $"{message}" : $"[{callerName.ToUpper()}] {message} : Ln {lineNumber}");
+
+        var fullCallerName = callerName;
+        foreach (var method in new StackTrace().GetFrames())
+        {
+            if (method.GetMethod().Name == callerName)
+            {
+                fullCallerName = $"{method.GetMethod().ReflectedType.Name}.{callerName}";
+            }
+        }
+
+        Mod.Logger.Msg(color, callerName == null ? $"{message}" : $"[{fullCallerName}] {message} : Ln {lineNumber}");
     }
 
-    internal static void VerboseLog(string message, [CallerMemberName] string callerName = null, [CallerLineNumber] int lineNumber = 0)
-    {
-        VerboseLog(ConsoleColor.DarkGray, callerName == null ? $"{message}" : $"[{callerName.ToUpper()}] {message} : Ln {lineNumber}");
-    }
+    internal static void VerboseLog(string message, [CallerMemberName] string callerName = null, [CallerLineNumber] int lineNumber = 0) =>
+        VerboseLog(ConsoleColor.DarkGray, message, callerName, lineNumber);
+
 }
