f7213e3d5fd116ecb01fe4958ac40b8540d6bf32
TheUltimateNuke <nuke.public@gmail.com>
2024-12-18 10:30:26 -0500

    added custom costumeobject loading from addressables

---
+++ CementMod/src/Patches/CustomCostumePatches.cs
@@ -0,0 +1,38 @@
+using System;
+using CementGB.Mod.Utilities;
+using HarmonyLib;
+using Il2CppCostumes;
+using UnityEngine.AddressableAssets;
+
+namespace CementGB.Mod.Patches;
+
+internal static class CustomCostumePatches
+{
+    [HarmonyPatch(typeof(CostumeDatabase), nameof(CostumeDatabase.Load))]
+    private static class CostumeDatabasePatch
+    {
+        private static void Postfix(CostumeDatabase __instance)
+        {
+            foreach (var location in AssetUtilities.GetAllModdedResourceLocationsOfType<CostumeObject>())
+            {
+                var handle = Addressables.LoadAsset<CostumeObject>(location).Acquire();
+                handle.WaitForCompletion();
+
+                if (handle.Status != UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+                {
+                    LoggingUtilities.VerboseLog(ConsoleColor.DarkRed, $"Failed to load custom Addressable costume object : Key \"{location.PrimaryKey}\" : OperationException {handle.OperationException?.ToString() ?? "null"}");
+                    continue; 
+                }
+
+                if (handle.Result == null)
+                {
+                    LoggingUtilities.VerboseLog(ConsoleColor.DarkRed, $"Custom Addressable costume object handle completed with no result : Key \"{location.PrimaryKey}\" : OperationException {handle.OperationException?.ToString() ?? "null"}");
+                    continue;
+                }
+
+                __instance.CostumeObjects.Add(handle.Result);
+                handle.Release();
+            }
+        }
+    }
+}
