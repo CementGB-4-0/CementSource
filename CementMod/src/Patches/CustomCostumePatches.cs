06920d0a23915d0622e074a6663b6c7812daadd1
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2025-01-05 12:13:23 -0500

    added logging and diagnostics to custom CostumeObject loading

--- CementMod/src/Patches/CustomCostumePatches.cs
+++ CementMod/src/Patches/CustomCostumePatches.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics;
 using CementGB.Mod.Utilities;
 using HarmonyLib;
 using Il2CppCostumes;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace CementGB.Mod.Patches;
 
@@ -26,14 +28,21 @@ internal static class CustomCostumePatches
     [HarmonyPatch(typeof(CostumeDatabase), nameof(CostumeDatabase.Load))]
     private static class CostumeDatabasePatch
     {
-        private static void Postfix(CostumeDatabase __instance)
+        private static void Postfix(ref Il2CppSystem.Collections.IEnumerator __result)
         {
+            if (__result.Current != null) return;
+
+            Mod.Logger.Msg("Injecting custom Addressable CostumeObjects into database. . .");
+            var timeTakenStopwatch = new Stopwatch();
+            var totalTimeTaken = new TimeSpan();
+            timeTakenStopwatch.Start();
+
             foreach (var location in AssetUtilities.GetAllModdedResourceLocationsOfType<CostumeObject>())
             {
                 var handle = Addressables.LoadAsset<CostumeObject>(location).Acquire();
                 handle.WaitForCompletion();
 
-                if (handle.Status != UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+                if (handle.Status != AsyncOperationStatus.Succeeded)
                 {
                     Mod.Logger.Error($"Failed to load custom Addressable CostumeObject : Key \"{location.PrimaryKey}\" : OperationException {handle.OperationException?.ToString() ?? "null"}");
                     handle.Release();
@@ -48,11 +57,17 @@ internal static class CustomCostumePatches
                 }
 
                 var res = handle.Result;
-                res._uid = __instance.NewUID();
-                __instance.CostumeObjects.Add(res);
+                res._uid = CostumeDatabase.Instance.NewUID();
+                CostumeDatabase.Instance.CostumeObjects.Add(res);
 
                 handle.Release();
+                totalTimeTaken.Add(timeTakenStopwatch.Elapsed);
+                Mod.Logger.Msg(ConsoleColor.DarkGreen, $"New custom CostumeObject registered : Key \"{location.PrimaryKey}\" : Time Taken {timeTakenStopwatch.Elapsed}");
+                timeTakenStopwatch.Restart();
             }
+
+            timeTakenStopwatch.Stop();
+            Mod.Logger.Msg(ConsoleColor.Green, $"Custom CostumeObject injection completed! Total time taken: {totalTimeTaken}");
         }
     }
 }
