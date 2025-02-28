51f5026c057f72176a65a6bffc5305299d4d3555
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2025-02-28 15:44:42 -0500

    refactor: rename GameObjectUtilities to UnityObjectUtilities

---
+++ CementMod/src/Utilities/UnityObjectUtilities.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace CementGB.Mod.Utilities;
+
+public static class UnityObjectUtilities
+{
+    public static void MakePersistent(this Object objec, bool dontDestroyOnLoad = false) => objec.hideFlags = HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
+}
