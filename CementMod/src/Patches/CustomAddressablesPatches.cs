602c12b65465ef02042ae04d4d48c336d02f3381
TheUltimateNuke <nuke.public@gmail.com>
2025-03-06 13:00:58 -0500

    fix: return false on LoadProviderPatch success

--- CementMod/src/Patches/CustomAddressablesPatches.cs
+++ CementMod/src/Patches/CustomAddressablesPatches.cs
@@ -1,5 +1,7 @@
 using CementGB.Mod.Utilities;
 using HarmonyLib;
+using Il2Cpp;
+using Il2CppSystem.IO;
 using MelonLoader.Utils;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -49,10 +51,50 @@ internal static class CustomAddressablesPatches
     [HarmonyPatch(typeof(AddressablesRuntimeProperties), nameof(AddressablesRuntimeProperties.EvaluateProperty))]
     internal static class RuntimePropertiesPatch
     {
-        private static bool Prefix(string name, ref string __result)
+        private static bool Prefix(AddressablesImpl addressables, ObjectInitializationData providerData, string providerSuffix)
         {
-            if (name != ModsDirectoryPropertyName) return true;
-            __result = MelonEnvironment.ModsDirectory;
+            if (MelonEnvironment.ModsDirectory == Path.Combine(Application.dataPath, "..", "Mods") || !providerData.Id.Contains(Path.Combine(Application.dataPath, "..", "Mods")))
+            {
+                return true;
+            }
+            
+            var indexOfExistingProvider = -1;
+            var newProviderId = string.IsNullOrEmpty(providerSuffix) ? providerData.Id.Replace($"{Application.dataPath}/../Mods", MelonEnvironment.ModsDirectory) : (providerData.Id.Replace($"{Application.dataPath}/../Mods", MelonEnvironment.ModsDirectory) + providerSuffix);
+            for (int i = 0; i < addressables.ResourceManager.ResourceProviders.Cast<ListWithEvents<IResourceProvider>>().Count; i++)
+            {
+                var rp = addressables.ResourceManager.ResourceProviders[i];
+                if (rp.ProviderId != newProviderId)
+                {
+                    continue;
+                }
+
+                indexOfExistingProvider = i;
+                break;
+            }
+
+            //if not re-initializing, just use the old provider
+            if (indexOfExistingProvider >= 0 && string.IsNullOrEmpty(providerSuffix))
+                return false;
+
+            var provider = providerData.CreateInstance<IResourceProvider>(newProviderId);
+            if (provider != null)
+            {
+                if (indexOfExistingProvider < 0 || !string.IsNullOrEmpty(providerSuffix))
+                {
+                    Addressables.LogFormat("Addressables - added provider {0} with id {1}.", provider.Cast<Il2CppSystem.Object>(), provider.ProviderId);
+                    addressables.ResourceManager.ResourceProviders.Cast<ListWithEvents<IResourceProvider>>().Add(provider);
+                }
+                else
+                {
+                    Addressables.LogFormat("Addressables - replacing provider {0} at index {1}.", provider.Cast<Il2CppSystem.Object>(), indexOfExistingProvider);
+                    addressables.ResourceManager.ResourceProviders[indexOfExistingProvider] = provider;
+                }
+            }
+            else
+            {
+                Addressables.LogWarningFormat("Addressables - Unable to load resource provider from {0}.", providerData);
+            }
+
             return false;
         }
     }
