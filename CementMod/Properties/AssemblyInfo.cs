21d19f9c386819e4afc7e03ba38d2f44767ca3a8
TheUltimateNuke <nuke.public@gmail.com>
2025-03-27 16:15:12 -0400

    made project rely on BuildInfo generated through props

--- CementMod/Properties/AssemblyInfo.cs
+++ CementMod/Properties/AssemblyInfo.cs
@@ -1,18 +1,15 @@
 ﻿using System.Reflection;
 using CementGB.Mod;
 using MelonLoader;
-using BuildInfo = CementGB.Mod.BuildInfo;
 
-[assembly: AssemblyTitle(BuildInfo.Description)]
-[assembly: AssemblyDescription(BuildInfo.Description)]
-[assembly: AssemblyCompany(BuildInfo.Company)]
-[assembly: AssemblyProduct(BuildInfo.Name)]
-[assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
-[assembly: AssemblyTrademark(BuildInfo.Company)]
-[assembly: AssemblyVersion(BuildInfo.Version)]
-[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: AssemblyCompany(MyPluginInfo.Author)]
+[assembly: AssemblyProduct(MyPluginInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + MyPluginInfo.Author)]
+[assembly: AssemblyTrademark(MyPluginInfo.Author)]
+[assembly: AssemblyVersion(MyPluginInfo.Version)]
+[assembly: AssemblyFileVersion(MyPluginInfo.Version)]
 
-[assembly: MelonInfo(typeof(Mod), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
+[assembly: MelonInfo(typeof(Mod), MyPluginInfo.Name, MyPluginInfo.Version, MyPluginInfo.Author, null)]
 [assembly: MelonColor(0, 99, 198, 255)]
 [assembly: MelonGame("Boneloaf", "Gang Beasts")]
 [assembly: MelonPriority(-1000)]
