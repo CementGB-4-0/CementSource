7e5c0a61289c9d1f443a12420f957b30698fd2ad
TheUltimateNuke <nuke.public@gmail.com>
2025-06-10 16:35:28 -0400

    redo CustomCostumePatches.cs

--- CementMod/src/Patches/CustomCostumePatches.cs
+++ CementMod/src/Patches/CustomCostumePatches.cs
@@ -1,78 +1,33 @@
+using System;
+using System.Diagnostics;
+using CementGB.Mod.CustomContent;
+using HarmonyLib;
+using Il2Cpp;
+
 namespace CementGB.Mod.Patches;
-/*
 internal static class CustomCostumePatches
 {
-    private static ushort NewUID(this CostumeDatabase __instance)
-    {
-        ushort num;
-        try
-        {
-            num = (ushort)(__instance.searchSpeeder.GetKey(__instance.searchSpeeder.Count - 1) + 1);
-        }
-        catch (OverflowException)
-        {
-            Mod.Logger.Error("There are too many CostumeObject UIDs!");
-            num = 0;
-        }
-
-        return num;
-    }
-
-    [HarmonyPatch(typeof(CostumeDatabase._Load_d__12), nameof(CostumeDatabase._Load_d__12.MoveNext))]
+    [HarmonyPatch(typeof(CostumesAssetBundleLoader._LoadCostumeDatabases_d__17), nameof(CostumesAssetBundleLoader._LoadCostumeDatabases_d__17.MoveNext))]
     private static class CostumeDatabasePatch
     {
-        private static void Postfix(bool __result, CostumeDatabase._Load_d__12 __instance)
+        static readonly Stopwatch timeTakenStopwatch = new Stopwatch();
+
+        private static void Postfix(bool __result, CostumesAssetBundleLoader._LoadCostumeDatabases_d__17 __instance)
         {
-            if (__result)
-            {
-                return;
-            }
-
+            if (__result) return;
+            
             Mod.Logger.Msg("Injecting custom Addressable CostumeObjects into vanilla databases. . .");
-            var timeTakenStopwatch = new Stopwatch();
             timeTakenStopwatch.Start();
+            CustomAddressableRegistration.InitializeCostumeReferences();
 
-            foreach (var location in AssetUtilities.GetAllModdedResourceLocationsOfType<CostumeObject>())
+            foreach (var costumeRef in CustomAddressableRegistration.CustomCostumes)
             {
-                var handle = Addressables.LoadAsset<CostumeObject>(location).Acquire();
-                handle.WaitForCompletion();
-
-                if (handle.Status != AsyncOperationStatus.Succeeded)
-                {
-                    Mod.Logger.Error(
-                        $"Failed to load custom Addressable CostumeObject : Key \"{location.PrimaryKey}\" : OperationException {handle.OperationException?.ToString() ?? "null"}");
-                    handle.Release();
-                    continue;
-                }
-
-                if (handle.Result == null)
-                {
-                    Mod.Logger.Error(
-                        $"Handle loading Custom CostumeObject completed with no result : Key \"{location.PrimaryKey}\" : OperationException {handle.OperationException?.ToString() ?? "null"}");
-                    handle.Release();
-                    continue;
-                }
-
-                var res = handle.Result;
-                res._uid = __instance.__4__this.NewUID();
-
-                if (res._uid == 0)
-                {
-                    return;
-                }
-
-                __instance.__4__this.CostumeObjects.Add(res);
-                __instance.__4__this.searchSpeeder.Add(res._uid, res);
-
-                handle.Release();
-                Mod.Logger.Msg(ConsoleColor.DarkGreen,
-                    $"New custom CostumeObject registered : Key \"{location.PrimaryKey}\"");
+                __instance.__4__this.CostumeDatabase.CostumeObjects.Add(costumeRef.Data);
+                __instance.__4__this.CostumeDatabase.searchSpeeder[costumeRef.Data._uid] = costumeRef.Data;
             }
-
+            
             timeTakenStopwatch.Stop();
-            Mod.Logger.Msg(ConsoleColor.Green,
-                $"Custom CostumeObject injection completed! Total time taken: {timeTakenStopwatch.Elapsed}");
+            Mod.Logger.Msg(ConsoleColor.Green, $"Done injecting custom CostumeObjects! Took {timeTakenStopwatch.ElapsedMilliseconds}ms");
         }
     }
 }
-*/
