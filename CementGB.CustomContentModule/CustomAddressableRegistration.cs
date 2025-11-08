e154b7294704e0de567a9bd23e958121e8251ede
TheUltimateNuke <nuke.public@gmail.com>
2025-11-08 16:07:14 -0500

    Revert "refactor: remove AddressableShaderCache as it seems obsolete"
    This reverts commit 47b6d997da61c790ad8d4cdca8d89f4e22cee430.

--- CementGB.CustomContentModule/CustomAddressableRegistration.cs
+++ CementGB.CustomContentModule/CustomAddressableRegistration.cs
@@ -162,6 +162,7 @@ public static class CustomAddressableRegistration
         CacheBaseGameAddressableKeys();
         InitializeContentCatalogs();
         InitializeMapReferences();
+        AddressableShaderCache.Initialize();
     }
 
     private static void CacheBaseGameAddressableKeys()
