d6b9433059a6075214468e1c01d6003f9ca45918
TheUltimateNuke <nuke.public@gmail.com>
2026-01-09 13:20:49 -0500

    fix: add back SceneAudioConfig defaulting to 4.3

--- src/CementGB.CustomContentModule/Loaders/CustomMaps/Patches/SceneLoadingPatches.cs
+++ src/CementGB.CustomContentModule/Loaders/CustomMaps/Patches/SceneLoadingPatches.cs
@@ -1,11 +1,32 @@
+using CementGB.Modules.CustomContent.Utilities;
 using HarmonyLib;
+using Il2CppAudio;
 using Il2CppGB.Core.Loading;
 using Il2CppGB.Data.Loading;
 using Il2CppTMPro;
+using UnityEngine;
 using ConsoleColor = System.ConsoleColor;
 using Resources = Il2CppGB.Core.Resources;
 
 namespace CementGB.Modules.CustomContent.Patches;
+
+[HarmonyPatch(typeof(SceneLoadTask), nameof(SceneLoadTask.OnDataLoaded))]
+internal static class SceneLoadTask_OnDataLoadedPatch
+{
+    private static void Postfix(SceneLoadTask __instance)
+    {
+        var sceneData = __instance._sceneData;
+        if (!sceneData._audioConfig)
+        {
+            sceneData._audioConfig = ScriptableObject.CreateInstance<SceneAudioConfig>();
+            sceneData._audioConfig.MakePersistent();
+        }
+
+        sceneData._audioConfig.audioMixer = MixerFinder.MainMixer;
+        if (Mathf.Approximately(sceneData._audioConfig.musicData.maxVolume, 1f))
+            sceneData._audioConfig.musicData.maxVolume = 0.15f;
+    }
+}
 
 [HarmonyPatch(typeof(SceneLoader), nameof(SceneLoader.OnSceneListComplete))]
 internal static class OnSceneListCompletePatch
