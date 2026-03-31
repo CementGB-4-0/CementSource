237c3b8429f3e6794afb68f8ad42b71038392fb9
TheUltimateNuke <nuke.public@gmail.com>
2026-03-31 13:45:23 -0400

    Allow launch options to override config values (-SERVER and -pfwd)

--- src/CementGB.NetBeardModule/NetBeardModule.cs
+++ src/CementGB.NetBeardModule/NetBeardModule.cs
@@ -67,8 +67,13 @@ public class NetBeardModule : InstancedCementModule
         else
         {
             CurrentConfig = TomletMain.To<NetBeardConfig>(File.ReadAllText(ConfigFilePath));
+            if (Environment.CommandLine.Contains(ArgConstants.ServerArg))
+                CurrentConfig.Dedicated = true;
+            if (Environment.CommandLine.Contains(ArgConstants.UpnpArg))
+                CurrentConfig.UpnpEnabled = true;
         }
 
+        //CementPreferences.ShouldSkipSplashes += () => IsServer;
         LobbyCommunicator.Awake();
         TCPCommunicator.Init();
         LobbyManager.add_onSetupComplete(new Action(OnBoot));
@@ -119,7 +124,7 @@ public class NetBeardModule : InstancedCementModule
         Logger?.Msg($"{ServerLogPrefix} Setting up server boot...");
         var bootstrapper = Object.FindObjectOfType<NetworkBootstrapper>();
         bootstrapper.AutoRunServer = IsServer && !DontAutoStart;
-        MonoSingleton<Global>.Instance.LevelLoadSystem.gameObject.SetActive(false);
+        //MonoSingleton<Global>.Instance.LevelLoadSystem.gameObject.SetActive(false);
         // NetMemberContext.LocalHostedGame = true;
         GameManagerNew.add_OnGameManagerCreated((Action)SetConfigOnGameManager);
         NetUtils.Model.Subscribe("SERVER_READY", (NetModelItem<NetInt>.ItemHandler)OnServerReady);
