966afd8656e5a39af4e5d6fddc04885a2d559bb5
TheUltimateNuke <nuke.public@gmail.com>
2026-04-09 07:44:46 -0400

    Add CementGB.HeadlessFixes

---
+++ src/CementGB.HeadlessFixes/Entrypoint.cs
@@ -0,0 +1,21 @@
+﻿using CementGB.HeadlessFixes;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Entrypoint), "CementGB.HeadlessFixes", "1.0.0", "CementGB Team")]
+[assembly: MelonGame("Boneloaf", "Gang Beasts")]
+
+namespace CementGB.HeadlessFixes;
+
+public class Entrypoint : MelonPlugin
+{
+    private static bool IsBatchMode => Environment.GetCommandLineArgs().Contains("-batchmode") ||
+                                       Environment.GetCommandLineArgs().Contains("-nographics");
+
+    public override void OnPreSupportModule()
+    {
+        if (!IsBatchMode) return;
+
+        FindMelon("UnityExplorer", "Sinai, yukieiji").Unregister();
+        FindMelon("UnityExplorer", "Sinai")?.Unregister();
+    }
+}
